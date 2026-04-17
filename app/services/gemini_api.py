0d1389ba5e4cc5371eff8bbe0d901cb87a8b2cec
Nguyen Thang <nguyenquocthang304@gmail.com>
2026-04-18 03:52:25 +0700

    update gemini 3.1 lite

--- app/services/gemini_api.py
+++ app/services/gemini_api.py
@@ -53,7 +53,7 @@ def get_ai_recommendation(metrics_data: dict, product_name: str):
     try:
         # Cú pháp gọi API theo thư viện mới
         response = client.models.generate_content(
-            model='gemini-2.5-pro',
+            model='gemini-3.1-flash-lite-preview',
             contents=prompt,
             config=types.GenerateContentConfig(
                 response_mime_type="application/json",
